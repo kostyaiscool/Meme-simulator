e274420bf2dd45a000b87e3b0679ba99e1dfa90a
kostyaiscool <yanubtitrupzaeal@gmail.com>
2025-08-23 12:37:22 +0300

    Пофиксил уебанский костыль с win32api

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
-import pygame
-import win32api
-import win32con
 import ctypes
 import locale
-width = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)
-height = win32api.GetSystemMetrics(win32con.SM_CYSCREEN)
-screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+import pygame as pg
+pg.init()
+info = pg.display.Info()
+width = info.current_w
+height = info.current_h
+print("Ширина:", width, "Высота:", height)
+screen = pg.display.set_mode((width, height), pg.RESIZABLE)
 lang_id = ctypes.windll.kernel32.GetUserDefaultUILanguage()
 lang_code = locale.windows_locale.get(lang_id)
 used_lang = lang_code.split("_")
 print(used_lang[0])
-pygame.display.set_caption("Мемный симулятор")
-pygame.display.set_icon(screen)
+pg.display.set_caption("Мемный симулятор")
+pg.display.set_icon(screen)
 # cube_width  = (25 / 480) * screen_width
 # cube_height = (25 / 360) * screen_height
